3d42760e3356c38aa4806d110fe45c2f4d1c4b6c
barisariburnu <barisariburnu@gmail.com>
2021-12-20 14:05:48 +0300

    Add mapbox integration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
     images: {
-        domains: ["links.papareact.com", "upload.wikimedia.org"],
+        domains: ["links.papareact.com", "upload.wikimedia.org"]
     },
+    env: {
+        mapbox_key:
+            "pk.eyJ1IjoiYmFyaXNhcmlidXJudSIsImEiOiJjaXQ5eG9rbHIwMDNsMnlwaDdoYzgyOW9vIn0.ds_j1qVWujprWMC5auOazg"
+    }
 };
